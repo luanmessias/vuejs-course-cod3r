5751900548e6dd70f0ee0f4152ed201ea50665a5
Luan Messias <luanmessias03@gmail.com>
2021-06-14 17:13:15 -0300

    add class 108 - component registry global and local

---
+++ 07_components/component-intro/src/Contador.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="contador">
+    <button @click="subtrair">-</button>
+    <span>{{ contador }}</span>
+    <button @click="adicionar">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0
+    }
+  },
+  methods: {
+    adicionar() {
+      this.contador++
+    },
+    subtrair() {
+      this.contador--
+    }
+  }
+}
+</script>
+
+<style>
+  .contador span {
+    border-bottom: 1px solid #CCC;
+    height: 30px;
+    padding: 5px 25px;
+  }
+
+  .contador button {
+    height: 30px;
+    width: 30px;
+    border-radius: 50%;
+    background-color: coral;
+    color: #FFF;
+    margin: 0px 5px;
+    outline: none;
+  }
+</style>
---
+++ 07_components/component-intro/src/Contadores.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="contadores">
+    <app-contador />
+    <app-contador />
+    <app-contador />
+    <app-contador />
+    
+  </div>
+</template>
+
+<script>
+import Contador from './Contador.vue'
+
+export default {
+  components: {
+    'app-contador': Contador,
+  }
+}
+</script>
+
+<style>
+
+</style>

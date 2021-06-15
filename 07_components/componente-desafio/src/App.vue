a93e3a8df58094123c6df8cfbca17910b30d228a
Luan Messias <luanmessias03@gmail.com>
2021-06-15 12:22:53 -0300

    add challenge files about components

---
+++ 07_components/componente-desafio/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <NavBar/>
+
+    <Header />
+
+    <v-content>
+       <Carousel />
+       <HelloWorld />
+    </v-content>
+
+   
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld/Component.vue'
+import Carousel from '@/components/Carousel/Component.vue'
+import NavBar from '@/components/NavBar/Component.vue'
+import Header from '@/components/Header/Component.vue'
+import Footer from '@/components/Footer/Component.vue'
+
+export default {
+components: {
+    HelloWorld,
+    Carousel,
+    NavBar,
+    Header,
+    Footer,
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>

183d680921cdcc69e35ac85ca8e9ca9c347c9347
Luan Messias <luanmessias03@gmail.com>
2021-06-15 19:34:11 -0300

    add classes about $emit and callback props

---
+++ 08_comunication/src/components/UsuarioEditar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="componente">
+        <h2>Alterar os Dados de Usuário</h2>
+        <p>Edite as informações</p>
+        <p>Idade do Usuario: <strong>{{ idade }}</strong></p>
+        <hr>
+        Add new age: <br>
+        <input type="text" v-model="age">
+        <hr>
+        <button @click="alterarIdade">Alterar Idade</button>
+        <button @click="changeAgeFn(Number(age))">Mudar Idade (Callback)</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        idade: Number,
+        changeAgeFn: Function,
+    },
+    data() {
+        return {
+            age: this.idade
+        }
+    },
+    methods: {
+        alterarIdade() {
+            //this.idade = 33
+            this.$emit('idadeMudou', 33)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #98b99a;
+        color: #fff;
+    }
+</style>

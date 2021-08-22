e216b5bd77db04ede909abb2dcbabce5da7e1e45
Luan Messias <luanmessias03@gmail.com>
2021-08-22 15:57:49 -0300

    Add files about animation class

--- 12_filtros_mixins/filtros-mixins-desafio/src/App.vue
+++ 12_filtros_mixins/filtros-mixins-desafio/src/App.vue
@@ -1,62 +1,65 @@
 <template>
-	<div id="app">
-		<h1>Filtros & Mixins (Desafio)</h1>
-		<!-- Exercício 1 -->
-		<!-- Construir um filtro local que troca espaços por vírgula -->
+  <div id="app">
+    <h1>Filtros & Mixins (Desafio)</h1>
+    <!-- Exercício 1 -->
+    <!-- Construir um filtro local que troca espaços por vírgula -->
 
-		<fieldset>
-			<legend>Filtro local que troca espaços por vírgula</legend>
-			<p>
-				{{ sentense | changeSpaceToComma  }}
-			</p>
-		</fieldset>
+    <fieldset>
+      <legend>Filtro local que troca espaços por vírgula</legend>
+      <p>
+        {{ sentense | changeSpaceToComma }}
+      </p>
+    </fieldset>
 
-		<fieldset>
-			<legend>[COMPUED] - Filtro local que troca espaços por vírgula</legend>
-			<p>
-				{{ changeSpaceToCommaComputed }}
-			</p>
-		</fieldset>
-		
-		<!-- Exercício 2 -->
-		<!-- Filtro global que conta o tamanho de cada palavra e adiciona o 
+    <fieldset>
+      <legend>[COMPUED] - Filtro local que troca espaços por vírgula</legend>
+      <p>
+        {{ changeSpaceToCommaComputed }}
+      </p>
+    </fieldset>
+
+    <!-- Exercício 2 -->
+    <!-- Filtro global que conta o tamanho de cada palavra e adiciona o 
 			valor na string final -->
-		<!-- "Pedro é legal" => "Pedro (5) é (1) legal (5)" -->
+    <!-- "Pedro é legal" => "Pedro (5) é (1) legal (5)" -->
 
-		<fieldset>
-			<legend>Conta o tamanho de cada palavra e adiciona o 
-			valor na string final</legend>
-			<p>
-				{{ sentense | countWordSize  }}
-			</p>
-		</fieldset>
-		<fieldset>
-			<legend>[COMPUED] - Conta o tamanho de cada palavra e adiciona o 
-			valor na string final</legend>
-			<p>
-				{{ countWordSizeComputed  }}
-			</p>
-		</fieldset>
+    <fieldset>
+      <legend>
+        Conta o tamanho de cada palavra e adiciona o valor na string final
+      </legend>
+      <p>
+        {{ sentense | countWordSize }}
+      </p>
+    </fieldset>
+    <fieldset>
+      <legend>
+        [COMPUED] - Conta o tamanho de cada palavra e adiciona o valor na string
+        final
+      </legend>
+      <p>
+        {{ countWordSizeComputed }}
+      </p>
+    </fieldset>
 
-		<!-- Exercício 3 -->
-		<!-- Implementar os exercicios 1 e 2 com propriedade computada -->
+    <!-- Exercício 3 -->
+    <!-- Implementar os exercicios 1 e 2 com propriedade computada -->
 
-		<!-- Exercício 4 -->
-		<!-- Compartilhe a propriedade computada via mixin -->
-	</div>
+    <!-- Exercício 4 -->
+    <!-- Compartilhe a propriedade computada via mixin -->
+  </div>
 </template>
 
 <script>
-import AppMixin from './AppMixin'
+import AppMixin from "./AppMixin";
 
 export default {
   mixins: [AppMixin],
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
